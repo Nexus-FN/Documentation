56a8848e382cf90e2e8a96b415f9bef3d897a5e6
Finn <ernzerhofffinn@gmail.com>
2023-05-17 00:30:07 +0200

    Updated documentation

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,17 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  faviconGlyph: '🚀',
+  logo: <span>Momentum</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Nexus-FN/Momentum',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/HuYyFYfwNp',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/Nexus-FN/Documentation',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Momentum Documentation',
   },
 }
 
